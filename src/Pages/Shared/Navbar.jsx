6e97e8e1bc68a04a0d46fc8f23218ead3391bc19
sabbirofc <mdsabbirhowlader420@gmail.com>
2023-11-10 17:24:52 +0600

    rating showed into testimonials

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   );
   return (
     <div>
-      <div className="navbar fixed bg-black bg-opacity-30 z-10 max-w-screen-xl text-white">
+      <div className="navbar fixed bg-black bg-opacity-30 z-10 max-w-screen-xl text-white ">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
